be6f9839fad394831f04933dc0013696715a2339
Maxwellusman <maxwelwritings@gmail.com>
2025-04-20 12:14:55 -0700

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,24 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        // Redirect www to non-www
+        source: "/:path*",
+        has: [{ type: "host", key: "host", value: "www.maxwritings.com" }],
+        destination: "https://maxwritings.com/:path*",
+        permanent: true,
+      },
+      {
+        // Redirect http to https
+        source: "/:path*",
+        has: [{ type: "protocol", key: "protocol", value: "http" }],
+        destination: "https://maxwritings.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
